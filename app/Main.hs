20edeb8f957ab04d48f7906d31fd71c6b23a1799
vvcaw <eric.elflein@googlemail.com>
2022-11-29 01:04:44 +0100

    Create and parse configuration file

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,25 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
 import Lib
-import Data.Map
+import System.Directory
 
 main :: IO ()
-main = evaluateCommandWithLabel (fromList [("list", "ls")]) "list"
+main = do
+  homeDir <- getHomeDirectory
+  -- Path to configuration file
+  let dirPath = homeDir ++ "/.config/jmp/"
+  let absoluteFilePath = dirPath ++ "jmp.list"
+  fileExists <- doesFileExist absoluteFilePath
+  -- Only act if file exists
+  if fileExists
+      -- Get the configuration and execute the given command if possible
+    then do
+      config <- configurationMap absoluteFilePath
+      evaluateCommandWithLabel config "demo"
+    else do
+      putStrLn $ "No config file found. Creating one in " ++ dirPath
+      createAndWriteFile dirPath absoluteFilePath "demo ls"
+      putStrLn
+        "New config file was created! Use `jmp demo` to execute the demo command: `ls`"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,28 @@
 module Lib
-    ( evaluateCommandWithLabel
-    ) where
+  ( evaluateCommandWithLabel
+  , configurationMap
+  , createAndWriteFile
+  ) where
 
-import System.Process
-import qualified Data.Map as M
 import qualified Data.Foldable as F
+import qualified Data.Map as M
+import System.Directory
+import System.Process
 
 evaluateCommandWithLabel :: M.Map String String -> String -> IO ()
 evaluateCommandWithLabel m s = F.forM_ command callCommand
-    where 
-        command = commandWithLabel m s
+  where
+    command = commandWithLabel m s
 
 commandWithLabel :: M.Map String String -> String -> Maybe String
 commandWithLabel m l = M.lookup l m
+
+configurationMap :: FilePath -> IO (M.Map String String)
+configurationMap path =
+  readFile path >>=
+  (return . M.fromList) . map ((\w -> (head w, last w)) . words) . lines
+
+createAndWriteFile :: FilePath -> FilePath -> String -> IO ()
+createAndWriteFile dirPath absoluteFilePath content = do
+  createDirectoryIfMissing True dirPath
+  writeFile absoluteFilePath content
